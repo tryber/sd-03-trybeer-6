cb39322a049cfef8b7b2de297138827c5ff57437
Gabriel <dreadpool38@gmail.com>
2020-09-21 17:46:27 -0300

    linter

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,13 @@
-require('dotenv').config()
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
+
 const app = express();
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const PORT = process.env.PORT||3001
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const PORT = process.env.PORT || 3001;
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-
-
-app.listen(PORT, () => console.log(`listen to port ${PORT} ` ));
+app.listen(PORT, () => console.log(`listen to port ${PORT} `));
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  
+
 }
 
 export default App;
