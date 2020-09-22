a30fdf516291e8dc4a80b6a02eb460d8b5f1ce29
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-09-22 09:55:52 -0300

    configurado eslint com prettier para facilitar formatação

--- front-end/src/components/login/Login.jsx
+++ front-end/src/components/login/Login.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
     <form>
-      <label htmlFor="email-input">E-mail:</label>
-      <input type="text" name="email-input" data-testid="email-input" />
-      <label htmlFor="password-input">Senha:</label>
-      <input type="password" name="password-input" data-testid="password-input" />
-      <button type="button" name="signin-btn" data-testid="signin-btn">Entrar</button>
+      <label htmlFor="email-input">
+        E-mail:
+        <input type="text" name="email-input" data-testid="email-input" />
+      </label>
+      <label htmlFor="password-input">
+        Senha:
+        <input
+          type="password"
+          name="password-input"
+          data-testid="password-input"
+        />
+      </label>
+      <button type="button" name="signin-btn" data-testid="signin-btn">
+        Entrar
+      </button>
       <Link to="/register" name="no-account-btn" data-testid="no-account-btn">
         Ainda não tenho conta
       </Link>
