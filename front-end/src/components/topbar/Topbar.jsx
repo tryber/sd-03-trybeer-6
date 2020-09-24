d69fe2dd682ea63ce3b4b0f021e6c2b9ed3a1351
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-09-24 18:54:09 -0300

    Criado menu mobile

--- front-end/src/components/topbar/Topbar.jsx
+++ front-end/src/components/topbar/Topbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsJustify } from 'react-icons/bs';
 
-export default function Topbar() {
+import './Topbar.css';
+
+export default function TopBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div>
+    <div className="menu-container">
+      <button
+        className="menu-drag-button"
+        type="button"
+        onClick={ () => setMenuOpen(!menuOpen) }
+      >
+        <BsJustify />
+      </button>
+      <h1 className="title">Trybeer</h1>
+      <div className={ menuOpen ? 'menu-open' : 'menu-closed' }>
+        <ul className="menu-ul">
+          <li className="menu-li">Produtos</li>
+          <li className="menu-li">Meus Pedidos</li>
+          <li className="menu-li">Meu Perfil</li>
+          <li className="menu-li">Sair</li>
+        </ul>
+      </div>
     </div>
   );
 }
