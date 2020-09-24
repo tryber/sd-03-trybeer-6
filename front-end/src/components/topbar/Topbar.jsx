62b10d0e86826f028935341f71cab30ca0b87d09
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-09-24 19:21:00 -0300

    Menu top bar

--- front-end/src/components/topbar/Topbar.jsx
+++ front-end/src/components/topbar/Topbar.jsx
@@ -6,15 +6,23 @@ import './Topbar.css';
 export default function TopBar() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="menu-container">
-      <button
-        className="menu-drag-button"
-        type="button"
-        onClick={ () => setMenuOpen(!menuOpen) }
-      >
-        <BsJustify />
-      </button>
-      <h1 className="title">Trybeer</h1>
+    <div>
+      <div className="menu-container">
+        <div className="parent-menu-div">
+          <div className="button-menu-div">
+            <button
+              className="menu-drag-button"
+              type="button"
+              onClick={ () => setMenuOpen(!menuOpen) }
+            >
+              <BsJustify />
+            </button>
+          </div>
+          <div className="title-menu-div">
+            <h1 className="title">Trybeer</h1>
+          </div>
+        </div>
+      </div>
       <div className={ menuOpen ? 'menu-open' : 'menu-closed' }>
         <ul className="menu-ul">
           <li className="menu-li">Produtos</li>
