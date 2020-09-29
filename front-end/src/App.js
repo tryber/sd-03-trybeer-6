3cfc593d0874762a8defe72efba374022cef3596
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-09-28 22:57:56 -0300

    Arrumado index

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages';
+import Register from './components/register/Register';
+import Profile from './components/profile/Profile';
 import store from './store';
 
 import './App.css';
@@ -10,7 +12,16 @@ const App = () => (
   <Provider store={ store }>
     <Router>
       <Switch>
-        <Route exact path="/login" component={ Login } />
+        <Route path="/login" component={ Login } />
+        <Route path="/register" component={ Register } />
+        <Route path="/products" component={ () => <div>Products Page</div> } />
+        <Route
+          path="/admin/orders"
+          component={ () => <div>Admin Orders Page</div> }
+        />
+        <Route path="/orders" component={ () => <div>Orders Page</div> } />
+        <Route path="/profile" component={ Profile } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     </Router>
   </Provider>
