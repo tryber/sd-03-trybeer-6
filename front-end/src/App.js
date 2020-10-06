7d0afb4a4e0853d45a32daeb55d68dc5b2f8cc11
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-10-06 15:49:25 -0300

    Requisito do Admin Side Bar concluido

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,9 +3,9 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Login, Products } from './pages';
+import AdminSideBar from './components/adminSideBar/adminSideBar';
 import Register from './components/register/Register';
 import Profile from './components/profile/Profile';
-import Topbar from './components/topbar/Topbar';
 
 import store from './store';
 
@@ -19,7 +19,7 @@ const App = () => (
         <Route path="/register" component={ Register } />
         <Route path="/products" component={ Products } />
         <Route path="/admin/orders">
-          <Topbar menuTitle="TryBeer" />
+          <AdminSideBar menuTitle="TryBeer" />
         </Route>
         <Route path="/orders" component={ () => <div>Orders Page</div> } />
         <Route path="/profile" component={ Profile } />
