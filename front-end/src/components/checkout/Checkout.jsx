39255e3360135f8dd13e8368c1501e2ef42cd708
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-10-05 21:43:03 -0300

    Criando função de deletar item da lista e atualizar local storage

--- front-end/src/components/checkout/Checkout.jsx
+++ front-end/src/components/checkout/Checkout.jsx
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { BsTrash } from 'react-icons/bs';
 
 import Topbar from '../topbar/Topbar';
 
-function Checkout({ cart }) {
-  const cartItens = [JSON.parse(localStorage.getItem('cartItens')) || cart];
+function Checkout() {
+  const [cartItens, setCartItens] = useState();
+  const magicNumber = 0;
 
-  console.log(cartItens);
+  const handleList = (val) => {
+    console.log(val);
+    const newItens = cartItens.filter(({ product }) => product !== val);
+    console.log(newItens);
+    localStorage.removeItem('cartItens');
+    localStorage.setItem('cartItens', JSON.stringify(newItens));
+    setCartItens(newItens);
+  };
+
+  useEffect(() => {
+    setCartItens(JSON.parse(localStorage.getItem('cartItens')));
+  }, []);
 
   return (
     <div>
       <Topbar menuTitle="Finalizar Pedido" />
       <h2>Produtos</h2>
       <ul>
-        {/* { cartItens.map((item) => (
-          <li key={item.[Skol laa ]}>
-
-          </li>
-        ))} */}
+        {cartItens
+          && cartItens.length > magicNumber
+          && cartItens.map(({ quantity, price, product }) => (
+            <li key={ product }>
+              <div key={ `${product}+${price}` }>
+                <span>{quantity}</span>
+                <span>{product}</span>
+                <span>{price * quantity}</span>
+                <span>{`(${price})`}</span>
+                <button type="button" value={ product } onClick={ (e) => handleList(e.currentTarget.value) }>
+                  <BsTrash />
+                </button>
+              </div>
+            </li>
+          ))}
       </ul>
     </div>
   );
 }
 
-const mapStateToProps = ({ cart }) => ({
-  cart,
-});
-
-export default connect(mapStateToProps)(Checkout);
+export default connect()(Checkout);
 
 /* {
-  cart: {
-    'Skol Lata 250ml': 1,
-    'Heineken 600ml': 2,
-    'Antarctica Pilsen 300ml': 1,
-    'Skol 269ml': 1
-  },
-  total: 21.88
+  cart: [
+    {
+      quantity: 1,
+      price: '2.19',
+      product: 'Skol 269ml'
+    },
+    {
+      quantity: 1,
+      price: '4.49',
+      product: 'Skol Beats Senses 313ml'
+    }
+  ]
 } */
