cdfb736ebb298c767f5642c611e49e5f8ce3c5eb
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-10-20 09:09:24 -0300

    resolvendo eslint

--- front-end/src/components/profile/Profile.test.js
+++ front-end/src/components/profile/Profile.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { waitFor } from '@testing-library/dom';
-import { cleanup, act } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// import { waitFor } from '@testing-library/dom';
+// import { cleanup, act } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import renderWithRouter from '../../utils';
 import Profile from './Profile';
